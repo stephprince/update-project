48651df2d2be85a0a3f99c7bbf4e983b656fc2cc
Steph Prince <stephanie.m.prince1@gmail.com>
2021-01-14 13:00:04 -0500

    update inputs

--- src/runall.m
+++ src/runall.m
@@ -3,12 +3,12 @@
 % this script will generate all the analyses for the hpc-pfc diamond maze analyses
 
 %% sets up animals, dates, directories
-animals = [9,10];
-excldates = []; incldates = [];
+indices.animals = [13];
+indices.excldates = []; indices.incldates = [];
 
 %gets data directories and indices
 dirs = getdefaultdirectorieshpcpfcinteractions;
-indices = getdefaultindiceshpcpfcinteractions(animals,dirs,excldates,incldates);
+indices = getdefaultindiceshpcpfcinteractions(indices,dirs);
 
 %set flags for what you want to do
 makenewfiles = 0; % this flag generates new data structures, when set to 0 it uses preexisting data structures
@@ -16,7 +16,7 @@ behavior = 1;
 
 %% perform behavioral analyses
 if behavior
-    getDiamondTrackBehavior(dirs, indices, makenewfiles); %note, behavior uses different indices then the rest of the analyses  because there are more dates outside of ephys days
+    getUpdateTaskBehavior(dirs, indices, makenewfiles); %note, behavior uses different indices then the rest of the analyses  because there are more dates outside of ephys days
 end
 
 %% rest of analyses here
