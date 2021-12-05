0acfc70fb7eb4b3b2bcf5b594ae3cfc7365c3b7e
Steph Prince <stephanie.m.prince1@gmail.com>
2021-12-05 10:54:30 -0500

    convert plotting notebook to scripts

---
+++ update_project/camera_sync/cam_plot_utils.py
@@ -0,0 +1,228 @@
+import cv2
+import math
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+
+from pathlib import Path
+
+def create_track_boundaries():
+    # establish track boundaries
+    coords = [[2, 1], [2, 245], [10, 260], [10, 280], [8, 280], [0.5, 267], [-0.5, 267], [-8, 280], [-10, 280],
+              [-10, 260], [-2, 245], [-2, 1], [2, 1]]
+    xs, ys = zip(*coords)
+
+    return xs, ys
+
+def extract_virmen_data(camera_inds_in_df, ind, interval, virmen_df):
+    # extract the current position
+    current_ind = camera_inds_in_df[ind]
+    x_pos = virmen_df.xPos[interval[0]:current_ind]
+    y_pos = virmen_df.yPos[interval[0]:current_ind]
+    x_pos[:5] = 0
+    y_pos[:5] = 5
+
+    # extract view angle
+    dx_angle = -math.cos(virmen_df.viewAngle[current_ind] + math.pi / 2)
+    dy_angle = math.sin(virmen_df.viewAngle[current_ind] + math.pi / 2)
+    view_angle_num = np.round(np.rad2deg(virmen_df.viewAngle[current_ind]))
+    view_angle = virmen_df.viewAngle[interval[0]:current_ind]
+
+    # extract time
+    time = virmen_df.time[interval[0]:current_ind]
+    trial_start_time = virmen_df.time[interval[0]]
+    trial_end_time = virmen_df.time[interval[-1]]
+    trial_duration = (trial_end_time - trial_start_time) * 60 * 60 * 24 # to convert to seconds
+    time_elapsed = (time - trial_start_time) * 60 * 60 * 24  # to convert to seconds
+
+    # extract lick data (if 1, lick detected)
+    licks = np.diff(virmen_df.numLicks[interval[0]:current_ind], prepend=virmen_df.numLicks[interval[0]])
+    lick_events = time_elapsed[licks == 1]
+
+    # extract velocity
+    rot_veloc = virmen_df.rotVeloc[interval[0]:current_ind]
+    trans_veloc = virmen_df.transVeloc[interval[0]:current_ind]
+
+    return {"x_pos": x_pos, "y_pos": y_pos, "lick_events": lick_events,
+            "dx_angle": dx_angle, "dy_angle": dy_angle, "view_angle": view_angle, "view_angle_num": view_angle_num,
+            "time_elapsed": time_elapsed, "trial_duration": trial_duration,
+            "rot_veloc": rot_veloc, "trans_veloc": trans_veloc,
+            }
+
+def plot_update_cam_all(camera_inds_in_df, video_dir, frame_no, ind, interval, virmen_df, itrial):
+    # read in the image
+    frame_filename = Path(f"{video_dir}/temp/trial{itrial}/frame{frame_no}.png")
+    img = mpimg.imread(frame_filename)
+
+    # extract relevant data
+    vr_data = extract_virmen_data(camera_inds_in_df, ind, interval, virmen_df)
+    track_bounds_xs, track_bounds_ys = create_track_boundaries()
+    fps = np.shape(camera_inds_in_df)[0]/vr_data["trial_duration"]
+
+    # plot the data
+    mosaic = """
+            BBAAAACC
+            BBAAAADD
+            BBEEFFGG
+            """
+    #plt.style.use('dark_background')
+    lwidth = 3
+    ax_dict = plt.figure(constrained_layout=True, figsize=(20, 10)).subplot_mosaic(mosaic)
+
+    ax_dict["A"].imshow(img)
+    ax_dict["A"].set_xticks([])
+    ax_dict["A"].set_yticks([])
+
+    ax_dict["B"].plot(vr_data["x_pos"], vr_data["y_pos"], color="steelblue", linewidth=lwidth)
+    ax_dict["B"].plot(track_bounds_xs, track_bounds_ys, color='black')
+    ax_dict["B"].set_title('Track Position', fontsize=20)
+    ax_dict["B"].set_ylim(0, 300)
+    ax_dict["B"].set_xlim(10.1, -10.1)
+    ax_dict["B"].axis('off')
+
+    ax_dict["C"].plot(vr_data["time_elapsed"], vr_data["rot_veloc"], color='slateblue', linewidth=lwidth)
+    ax_dict["C"].set_xlabel('Time (s)', fontsize=14)
+    ax_dict["C"].set_ylabel('Speed (au)', fontsize=14)
+    ax_dict["C"].set_title('Rotational velocity', fontsize=20)
+    ax_dict["C"].set_xlim(0, vr_data["trial_duration"])
+    ax_dict["C"].set_ylim(-2.75, 2.75)
+
+    ax_dict["D"].plot(vr_data["time_elapsed"], vr_data["trans_veloc"], color='darkviolet', linewidth=lwidth)
+    ax_dict["D"].set_xlabel('Time (s)', fontsize=14)
+    ax_dict["D"].set_ylabel('Speed (au)', fontsize=14)
+    ax_dict["D"].set_title('Translational velocity', fontsize=20)
+    ax_dict["D"].set_xlim(0, vr_data["trial_duration"])
+    ax_dict["D"].set_ylim(0, 60)
+
+    ax_dict["E"].eventplot(vr_data["lick_events"], color="hotpink", linewidths=2)
+    ax_dict["E"].set_title('Licking activity', fontsize=20)
+    ax_dict["E"].set_xlim(0, vr_data["trial_duration"])
+    ax_dict["E"].axis('off')
+
+    ax_dict["F"].arrow(0, 0, vr_data["dx_angle"], vr_data["dy_angle"], head_width=0.1, head_length=0.1, linewidth=lwidth,
+                       color='seagreen')
+    ax_dict["F"].set_title('View Angle', fontsize=20)
+    ax_dict["F"].set_xlim(-1.25, 1.25)
+    ax_dict["F"].set_ylim(0, 1.25)
+    ax_dict["F"].axis('off')
+    ax_dict["F"].text(0.85, 0.85, f"{vr_data['view_angle_num']}\N{DEGREE SIGN}", fontsize=14)
+
+    ax_dict["G"].plot(vr_data["time_elapsed"], vr_data["view_angle"], color='seagreen', linewidth=lwidth)
+    ax_dict["G"].set_title('View Angle', fontsize=20)
+    ax_dict["G"].set_xlabel('Time (s)', fontsize=14)
+    ax_dict["G"].set_ylabel('View Angle', fontsize=14)
+    ax_dict["G"].set_ylim(-1.5, 1.5)
+    ax_dict["G"].set_xlim(0, vr_data["trial_duration"])
+
+    # save the entire plot for each camera frame
+    plot_filename = Path(f"{video_dir}/results/camera/trial{itrial}/trial{itrial}_frame{frame_no}_all.png")
+    plt.savefig(plot_filename)
+    plt.close()
+
+    return plot_filename, fps
+
+def plot_update_cam_simple(camera_inds_in_df, video_dir, frame_no, ind, interval, virmen_df, itrial):
+    # read in the image
+    frame_filename = Path(f"{video_dir}/temp/trial{itrial}/frame{frame_no}.png")
+    img = mpimg.imread(frame_filename)
+
+    # extract relevant data
+    vr_data = extract_virmen_data(camera_inds_in_df, ind, interval, virmen_df)
+    track_bounds_xs, track_bounds_ys = create_track_boundaries()
+
+    # plot the data
+    mosaic = """
+            BAAAAA
+            BAAAAA
+            BAAAAA
+            BAAAAA
+            BAAAAA
+            BCCCCC
+            """
+    lwidth = 3
+    ax_dict = plt.figure(constrained_layout=True, figsize=(20, 14)).subplot_mosaic(mosaic)
+
+    ax_dict["A"].imshow(img)
+    ax_dict["A"].set_xticks([])
+    ax_dict["A"].set_yticks([])
+
+    ax_dict["B"].plot(vr_data["x_pos"], vr_data["y_pos"], color="steelblue", linewidth=lwidth)
+    ax_dict["B"].plot(track_bounds_xs, track_bounds_ys, color='black')
+    ax_dict["B"].set_title('Track Position', fontsize=20)
+    ax_dict["B"].set_ylim(0, 300)
+    ax_dict["B"].set_xlim(10.1, -10.1)
+    ax_dict["B"].axis('off')
+
+    ax_dict["C"].eventplot(vr_data["lick_events"], color="hotpink", linewidths=lwidth)
+    ax_dict["C"].set_ylabel('Lick events', fontsize=14)
+    ax_dict["C"].set_xlabel('Time elapsed (s)', fontsize=14)
+    ax_dict["C"].set_xlim(0, vr_data["trial_duration"])
+
+    # save the entire plot for each camera frame
+    plot_filename = Path(f"{video_dir}/results/camera/trial{itrial}/trial{itrial}_frame{frame_no}_simple.png")
+    plt.savefig(plot_filename)
+    plt.close()
+
+    return plot_filename
+
+def plot_update_cam_angle_and_veloc(camera_inds_in_df, video_dir, frame_no, ind, interval, virmen_df, itrial):
+    # read in the image
+    frame_filename = Path(f"{video_dir}/temp/trial{itrial}/frame{frame_no}.png")
+    img = mpimg.imread(frame_filename)
+
+    # extract relevant data
+    vr_data = extract_virmen_data(camera_inds_in_df, ind, interval, virmen_df)
+    track_bounds_xs, track_bounds_ys = create_track_boundaries()
+
+    # plot the data
+    mosaic = """
+                AAAAAABB
+                AAAAAACC
+                """
+    lwidth = 3
+    ax_dict = plt.figure(constrained_layout=True, figsize=(20, 10)).subplot_mosaic(mosaic)
+
+    ax_dict["A"].imshow(img)
+    ax_dict["A"].set_xticks([])
+    ax_dict["A"].set_yticks([])
+
+    ax_dict["B"].plot(vr_data["time_elapsed"], vr_data["rot_veloc"], color='slateblue', linewidth=lwidth)
+    ax_dict["B"].set_xlabel('Time (s)', fontsize=14)
+    ax_dict["B"].set_ylabel('Speed (au)', fontsize=14)
+    ax_dict["B"].set_title('Rotational velocity', fontsize=20)
+    ax_dict["B"].set_xlim(0, vr_data["trial_duration"])
+    ax_dict["B"].set_ylim(-2.75, 2.75)
+
+    ax_dict["C"].plot(vr_data["time_elapsed"], vr_data["view_angle"], color='darkviolet', linewidth=lwidth)
+    ax_dict["C"].set_title('View Angle', fontsize=20)
+    ax_dict["C"].set_xlabel('Time (s)', fontsize=14)
+    ax_dict["C"].set_ylabel('View Angle', fontsize=14)
+    ax_dict["C"].set_ylim(-1.5, 1.5)
+    ax_dict["C"].set_xlim(0, vr_data["trial_duration"])
+
+    # save the entire plot for each camera frame
+    plot_filename = Path(f"{video_dir}/results/camera/trial{itrial}/trial{itrial}_frame{frame_no}_angle_veloc.png")
+    plt.savefig(plot_filename)
+    plt.close()
+
+    return plot_filename
+
+def write_camera_video(out_dir, fps, plot_filenames, vid_filename):
+    video_filename = Path(f"{out_dir}/{vid_filename}.mp4")
+    frame = cv2.imread(str(plot_filenames[0]))
+    height, width, layers = frame.shape
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video = cv2.VideoWriter(str(video_filename), fourcc, fps, (width, height))
+    for filename in plot_filenames:
+        video.write(cv2.imread(str(filename)))
+
+    cv2.destroyAllWindows()
+    video.release()
+
+def write_camera_gif(out_dir, plot_filenames, vid_filename):
+    # build gif
+    gif_filename = Path(f"{out_dir}/trial{itrial}_{vid_filename}.gif")
+    with imageio.get_writer(gif_filename, mode='I') as writer:
+        for filename in plot_filenames:
+            image = imageio.imread(filename)
+            writer.append_data(image)
---
+++ update_project/camera_sync/run_camera_syncing.py
@@ -0,0 +1,99 @@
+# import libraries
+import cv2
+import glob
+import numpy as np
+import pandas as pd
+import warnings
+
+from pathlib import Path
+
+from cam_utils import convert_mat_file_to_dict, get_trial_intervals
+from cam_plot_utils import plot_update_cam_all, plot_update_cam_simple, plot_update_cam_angle_and_veloc, write_camera_video
+
+# set inputs
+animal = 29
+date = 211027
+session = 1
+trial_type = "update"
+
+save_new_video_images = 0 # set to 1 to overwrite existing video frame temp files
+
+base_path = "y:/" #"/Volumes/labs/"
+video_dir = Path(f"{base_path}singer/CameraData/UpdateTask/S{animal}_{date}/")
+virmen_filename = Path(f"{base_path}singer/Virmen Logs/UpdateTask/S{animal}_{date}_{session}/virmenDataRaw.mat")
+output_dir = Path(f"{base_path}singer/CameraData/UpdateTask/S{animal}_{date}/{trial_type}/")
+
+# import virmen data
+if virmen_filename.is_file():
+    virmen_data = convert_mat_file_to_dict(virmen_filename)
+    virmen_df = pd.DataFrame(virmen_data['virmenData']['data'], columns=virmen_data['virmenData']['dataHeaders'])
+else:
+    warnings.warn('File can not be found')
+
+# import video frames
+video_file = video_dir / "*.mp4"
+video_file = str(video_file)
+video_file = glob.glob(video_file)[session-1]
+cap = cv2.VideoCapture(video_file)
+
+# check that the virmen data and the camera data have the same number of frames so that we can properly align them
+video_frame_count= int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+virmen_frame_count = sum(virmen_df.cameraTrigger)
+
+assert video_frame_count == virmen_frame_count
+
+# get trial intervals
+camera_trig = virmen_df.index[virmen_df.cameraTrigger == 1].to_list()
+trial_intervals = get_trial_intervals(virmen_df, camera_trig, trial_type)
+
+# save new video images
+if save_new_video_images:
+    # save images for each frame from those trials
+    for i, interval in enumerate(trial_intervals):
+        print(f"Saving frames for trial {i}")
+        # create folder
+        Path(f"{output_dir}/temp/trial{i}/").mkdir(parents=True, exist_ok=True)
+        img_dir = Path(f"{video_dir}/temp/{trial_type}/trial{i}/")
+
+        camera_frames = np.where(np.logical_and(camera_trig > interval[0], camera_trig < interval[1]))[0]
+        frame_no = camera_frames[0]
+
+        cap.set(cv2.CAP_PROP_POS_FRAMES,frame_no)
+        while frame_no <= camera_frames[-1]:
+            ret, frame = cap.read()
+            name = Path(f"{img_dir}/frame{frame_no}.png")
+            name = str(name)
+            cv2.imwrite(name, frame)
+            frame_no += 1
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+# use images to create movie
+for i, interval in enumerate(trial_intervals):
+    #get relevant trial data
+    trial_mask = np.logical_and(camera_trig > interval[0], camera_trig < interval[1])
+    camera_frames = np.where(trial_mask)[0]
+    camera_inds_in_df = np.array(camera_trig)[trial_mask]
+
+    # make plot for each camera frame
+    print(f"Making gif for trial {i}")
+    Path(f"{video_dir}/results/camera/{trial_type}/trial{i}/").mkdir(parents=True, exist_ok=True)
+    out_dir = Path(f"{video_dir}/results/camera/{trial_type}/trial{i}/")
+    plot_filenames_all = []
+    plot_filenames_simple = []
+    plot_filenames_angle_veloc = []
+    for ind, frame_no in enumerate(camera_frames):
+        filename, fps = plot_update_cam_all(camera_inds_in_df, video_dir, frame_no, ind, interval, virmen_df, i)
+        plot_filenames_all.append(filename)
+
+        filename = plot_update_cam_simple(camera_inds_in_df, video_dir, frame_no, ind, interval, virmen_df, i)
+        plot_filenames_simple.append(filename)
+
+        filename = plot_update_cam_angle_and_veloc(camera_inds_in_df, video_dir, frame_no, ind, interval, virmen_df, i)
+        plot_filenames_angle_veloc.append(filename)
+
+    # make videos for each plot type
+    write_camera_video(out_dir, fps, plot_filenames_all, 'all')
+    write_camera_video(out_dir, fps, plot_filenames_simple, 'simple')
+    write_camera_video(out_dir, fps, plot_filenames_angle_veloc, 'angle_veloc')
