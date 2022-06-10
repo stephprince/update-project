e3648b30168e102ade4e919a890444ffac10f34b
Steph Prince <stephanie.m.prince1@gmail.com>
2022-06-10 17:32:28 -0400

    fix conditions for empty stats output

--- update_project/statistics.py
+++ update_project/statistics.py
@@ -4,7 +4,7 @@ from scipy.stats import sem
 
 
 def get_fig_stats(data, axis=0):
-    if data.any():  # if not empty
+    if len(data):  # if not empty
         mean = np.nanmean(data, axis=axis)
         err = sem(data, axis=axis)
         upper = mean + 2 * err
@@ -14,6 +14,7 @@ def get_fig_stats(data, axis=0):
         err = []
         upper = []
         lower = []
+        # nan_arr = np.empty(np.delete(np.shape(data), axis))
 
     stats_dict = dict(mean=mean,
                       err=err,
