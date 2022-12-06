27da22d77e6c24593e540f4391ea2fbe8c2f5c4c
Steph Prince <stephanie.m.prince1@gmail.com>
2022-12-06 16:24:24 -0500

    work in progress - initial implementation of run all script to generate manuscript figures

---
+++ update_project/run_all_figures.py
@@ -0,0 +1,19 @@
+from update_project.general.session_loader import SessionLoader
+from update_task_analyzer import UpdateTaskAnalyzer
+
+overwrite = False  # whether or not to overwrite existing data structures
+animals = [17, 20, 25, 28, 29, 33, 34]
+dates_included = [210913]
+session_db = SessionLoader(animals=animals, dates_included=dates_included)
+
+# initialize general analysis class and run for each of the specificed steps
+analyzer = UpdateTaskAnalyzer(analysis=dict(Behavior=dict(),
+                                            ChoiceEstimate=dict(target_var='choice'),
+                                            Decoder=dict(features=['y_position', 'x_position', 'dynamic_choice'], ),
+                                            SingleUnits=dict()),
+                              sessions=session_db,
+                              overwrite=overwrite)
+
+# generate all manuscript figures
+analyzer.plot_main_figures()
+analyzer.plot_supplemental_figures()
